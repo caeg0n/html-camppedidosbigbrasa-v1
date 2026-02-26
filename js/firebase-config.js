fb71c3fadcc69fcf41ec5548710d81f3b7ef9ea1
caeg0n <caegon2005@hotmail.com>
2026-02-26 10:46:00 -0300

    Deploy cardapio web

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+﻿window.CAMPP_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDGXTuOE2blfbjof9GCzB7GI9icmA42HhM",
+  authDomain: "camp-pedidos.firebaseapp.com",
+  projectId: "camp-pedidos",
+  storageBucket: "camp-pedidos.firebasestorage.app",
+  messagingSenderId: "47375572573",
+  appId: "1:47375572573:web:65a4e23d592cbdc45477b4",
+  measurementId: ""
+};
+
+window.CAMPP_APP_CHECK_SITE_KEY = "6Lfi5XcsAAAAAGymqsG45Rd2RyMvKXHzO8EAjxOE";
+window.CAMPP_APP_KEY = "html-camppedidosbigbrasa-v1";
+
